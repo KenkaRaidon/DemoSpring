c7007e2093d4048d25343d571c25dff8795c7172
Eliezer Ontiveros <eliezer.ontiveros@uabc.edu.mx>
2022-06-23 12:58:43 -0700

    get set

---
+++ src/main/java/com/example/demo/Movies.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Movies {
+    String Title;
+    String Sinopsis;
+
+    public String getTitle(){
+        return Title;
+    }
+    public String getSinopsis(){
+        return Sinopsis;
+    }
+
+    public void SetTitle(String Title){
+        this.Title=Title;
+    }
+    public void SetSinopsis(String Sinopsis){
+        this.Sinopsis=Sinopsis;
+    }
+
+    
+}
